63e8bdc3dbdecb741413e0c8a2f7540e58497f31
DaltonMcCleery <dmccleery@apsolutions.net>
2018-08-20 14:07:40 -0500

    Update User's table in DB

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('real_name');
+            $table->string('username');
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('avatar');
+            $table->string('role')->default('ninja');
             $table->rememberToken();
             $table->timestamps();
         });
