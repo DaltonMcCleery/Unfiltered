2435683ae0a3d64121ae67352350938fdcc92e9a
DaltonMcCleery <dmccleery@apsolutions.net>
2018-08-21 08:36:20 -0500

    Find method in Game Controller

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Games;
+use App\Http\Resources\GamesResource;
 use Illuminate\Http\Request;
 
 class GameController extends Controller
@@ -14,4 +16,27 @@ class GameController extends Controller
     public function index() {
         return view('game.find');
     }
+
+    /**
+     * Get the currently available Games List (Public Games)
+     *
+     * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
+     */
+    public function find() {
+        $games = Games::where('type', 'public')->latest()->get();
+        return GamesResource::collection($games);
+    }
+
+    public function postQuestion(Request $request) {
+        //
+    }
+
+    public function postAnswer(Request $request) {
+        //
+    }
+
+    public function decideWinner(Request $request) {
+        //
+    }
+
 }
