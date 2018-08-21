0b9218f044c988c9bde4d476eff9bc6fc216cdf3
DaltonMcCleery <dmccleery@apsolutions.net>
2018-08-21 08:35:54 -0500

    Register new API routes for playing the Game

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,7 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::resource('game', 'GameController')
+    ->only(['postQuestion', 'postAnswer', 'decideWinner']);
+
+Route::get('games/find', 'GameController@find');
