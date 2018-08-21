70e6a6698dff29d5d07969bc3c1b5319f93cdbe1
DaltonMcCleery <dmccleery@apsolutions.net>
2018-08-21 09:06:41 -0500

    Update Find blade to include the same styling as the homepage

--- resources/views/game/find.blade.php
+++ resources/views/game/find.blade.php
@@ -1,4 +1,17 @@
 @extends('wrapper')
 @section('content')
-    <available-games></available-games>
+    <section class="hero is-medium is-dark is-bold">
+        <div class="hero-body">
+            <div class="container" align="center">
+                <h2 class="subtitle">
+                    <span class="icon is-medium">
+                        <i class="fas fa-exclamation-circle"></i>
+                    </span>
+                    AVAILABLE GAMES
+                </h2>
+                <br>
+                <available-games></available-games>
+            </div>
+        </div>
+    </section>
 @endsection
