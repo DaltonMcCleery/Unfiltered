4f9f424f930bf8bd93fbcf72d9bee9a2c338a636
DaltonMcCleery <dmccleery@apsolutions.net>
2018-08-24 09:40:07 -0500

    Simple error pages

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,15 @@
+@extends('wrapper')
+@section('content')
+    <section class="hero is-medium is-dark is-bold">
+        <div class="container" align="center">
+
+            {{-- Create a New Game --}}
+            <div class="hero-body">
+                <h1 class="title">404</h1>
+                <h2 class="subtitle">
+                    Yeah, hat probably doesn't exist yet...
+                </h2>
+            </div>
+        </div>
+    </section>
+@endsection
---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,15 @@
+@extends('wrapper')
+@section('content')
+    <section class="hero is-medium is-dark is-bold">
+        <div class="container" align="center">
+
+            {{-- Create a New Game --}}
+            <div class="hero-body">
+                <h1 class="title">500</h1>
+                <h2 class="subtitle">
+                    Don't worry, The Ninja Master has been alerted!
+                </h2>
+            </div>
+        </div>
+    </section>
+@endsection
