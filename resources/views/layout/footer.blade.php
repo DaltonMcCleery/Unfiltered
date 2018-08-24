1d8a5784e48fef011337fd670428a74bfb14adf9
DaltonMcCleery <dmccleery@apsolutions.net>
2018-08-24 09:56:32 -0500

    jQuery function for opening and closing the navbar's menu

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -9,4 +9,18 @@
 </footer>
 
 <script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
+<script>
+    // Navbar Menu Toggle
+    $(document).ready(function() {
+
+        // Check for click events on the navbar burger icon
+        $(".navbar-burger").click(function() {
+
+            // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+            $(".navbar-burger").toggleClass("is-active");
+            $(".navbar-menu").toggleClass("is-active");
+
+        });
+    });
+</script>
 @yield('additional-scripts')
